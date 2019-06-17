b8342889f8306bba1bd9614592ab8cb54e34fade
t0rben <45014600+rootorben@users.noreply.github.com>
2019-06-17 13:24:00 +0200

    Update and rename lscript to lscript.sh

---
+++ lscript.sh
@@ -0,0 +1,1 @@
+sudo mkdir /usr/share/lscript && sudo git clone https://github.com/arismelachroinos/lscript.git /usr/share/lscript/ && sudo chmod +x /usr/share/lscript/install.sh && sudo bash /usr/share/lscript/install.sh
