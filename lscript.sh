971dadcaf4e4f56a75b2d083d0d470b6f3dcf8f9
t0rben <45014600+rootorben@users.noreply.github.com>
2019-06-20 11:13:26 +0200

    Update lscript.sh

--- lscript.sh
+++ lscript.sh
@@ -1,1 +1,10 @@
+# LSCRIPT
 sudo mkdir /usr/share/lscript && sudo git clone https://github.com/arismelachroinos/lscript.git /usr/share/lscript/ && sudo chmod +x /usr/share/lscript/install.sh && sudo bash /usr/share/lscript/install.sh
+# KOADIC
+sudo git clone https://github.com/zerosum0x0/koadic.git /usr/share/koadic && sudo pip3 install -r /usr/share/koadic/requirements.txt && sudo /usr/share/koadic/koadic
+# KATANA
+sudo git clone https://github.com/PowerScript/KatanaFramework.git /usr/share/katana && sudo sh /usr/share/katana/dependencies && sudo python /usr/share/katana/install
+# WireSpy
+sudo git clone https://github.com/AresS31/wirespy /usr/share/wirespy && sudo chmod +x /usr/share/wirespy/* && sudo /usr/share/wirespy/wirespy.sh
+# PS1ENCODE
+sudo git clone https://github.com/CroweCybersecurity/ps1encode /usr/share/ps1encode
